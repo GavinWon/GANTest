4d94084132fcecb7d3b0aabd1eddd75abe21f53d
Gavin <gavinmwong@gmail.com>
2020-07-20 20:44:49 -0700

    The GAN Model

--- GAN.py
+++ GAN.py
@@ -69,3 +69,57 @@ def build_generator(start_filters, filter_size, latent_dim):
   x = Conv2D(3, kernel_size=5, padding='same', activation='tanh')(x)
   
   return Model(inputs=inp, outputs=x)
+
+#The GAN
+import pandas as pd
+import os
+from keras.optimizers import Adam
+
+# load celebrity images attributes
+df_celeb = pd.read_csv('list_attr_celeba.csv')
+TOTAL_SAMPLES = df_celeb.shape[0]
+
+# we will downscale the images
+SPATIAL_DIM = 64 
+# size of noise vector
+LATENT_DIM_GAN = 100 
+# filter size in conv layer
+FILTER_SIZE = 5
+# number of filters in conv layer
+NET_CAPACITY = 16
+# batch size
+BATCH_SIZE_GAN = 32
+# interval for displaying generated images
+PROGRESS_INTERVAL = 80 
+# directory for storing generated images
+ROOT_DIR = 'visualization'
+if not os.path.isdir(ROOT_DIR):
+    os.mkdir(ROOT_DIR)
+    
+
+
+def construct_models(verbose=False):
+    ### discriminator
+    discriminator = build_discriminator(NET_CAPACITY, SPATIAL_DIM, FILTER_SIZE)
+    # compile discriminator
+    discriminator.compile(loss='binary_crossentropy', optimizer=Adam(lr=0.0002), metrics=['mae'])
+
+    ### generator
+    # do not compile generator
+    generator = build_generator(NET_CAPACITY, FILTER_SIZE, LATENT_DIM_GAN)
+
+    ### DCGAN 
+    gan = Sequential()
+    gan.add(generator)
+    gan.add(discriminator)
+    discriminator.trainable = False 
+    gan.compile(loss='binary_crossentropy', optimizer=Adam(lr=0.0002), metrics=['mae'])
+
+    if verbose: 
+        generator.summary()
+        discriminator.summary()
+        gan.summary()
+        
+    return generator, discriminator, gan
+  
+generator_celeb, discriminator_celeb, gan_celeb = construct_models(verbose=True)
